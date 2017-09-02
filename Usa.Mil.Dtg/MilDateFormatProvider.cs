abe259bf26f38f7430424545fe9519b4438d1025
mflorez <mflorez@gmail.com>
2017-09-02 10:31:55 -0400

    Sting format provider implementation

--- Usa.Mil.Dtg/MilDateFormatProvider.cs
+++ Usa.Mil.Dtg/MilDateFormatProvider.cs
@@ -1,21 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Usa.Mil.Dtg
 {
-    class MilDateFormatProvider : IFormatProvider, ICustomFormatter
+    /// <summary>
+    /// Custom IMilDate formatter.  Use "dtz" to insert military time zone abbreviation.  
+    /// MilDateFormatProvider uses same string formats as System.DateTimeOffset.
+    /// </summary>
+    public class MilDateFormatProvider : IFormatProvider, ICustomFormatter
     {
         public object GetFormat(Type formatType)
         {
             return (formatType == typeof(ICustomFormatter)) ? this : null;
         }
 
+        /// <summary>
+        /// Adds the "dtz" string value to allow for Military Date Time Group (DTG) string formats.
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="arg"></param>
+        /// <param name="formatProvider"></param>
+        /// <returns></returns>
         public string Format(string format, object arg, IFormatProvider formatProvider)
         {
-            throw new NotImplementedException();
+            string mildateString = string.Empty;
+            string thisFmt = String.Empty;
+
+            if (!String.IsNullOrEmpty(format))
+                thisFmt = format;
+
+            if (arg is IMilDate)
+            {
+                string dtgTimeZone = "dtz";
+                string replaceString = "***";
+                IMilDate milDate = arg as IMilDate;
+                DateTimeOffset? dto = milDate.MilDateOffset;
+                mildateString = dto.Value.ToString(thisFmt.Replace(dtgTimeZone, replaceString)).Replace(replaceString, milDate.MilTimeZone.Abbreviation);
+            }
+            else
+            {
+                try
+                {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(String.Format("The format of '{0}' is invalid.", format), e);
+                }
+            }
+
+            return mildateString;                
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
         }
 
     }
---
+++ Usa.Mil.DtgTests/MilDateFormatProviderTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Usa.Mil.Dtg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usa.Mil.Dtg.Tests
+{
+    [TestClass()]
+    public class MilDateFormatProviderTests
+    {
+        [TestMethod()]
+        public void FormatTest()
+        {
+            string dtgString = "07142509 Z OCT 2017";
+            string format = "{0:ddHHmmss dtz MMM yyyy}";
+            IMilDate mdto = DateTimeMil.GetMilDateFromString(dtgString);
+            if (mdto.MilDateOffset.HasValue)
+            {
+                string mdtoString = String.Format(new MilDateFormatProvider(), format, mdto).ToUpper();
+                Assert.AreEqual(dtgString, mdtoString);
+            }
+        }
+    }
+}
